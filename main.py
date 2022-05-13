783d2157babb080ed6c2f130393bf7f46ab338ed
monkaCode <f.h612@hotmail.com>
2022-05-13 20:01:12 +0200

    Co-authored-by: mehmet802 <mehmet802@users.noreply.github.com> Co-authored-by: Plirexic <Plirexic@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import discord
 from discord.ext import commands
 import json
 
-with open("guilds.json") as f:
-    guildsJson = json.load(f)
+# with open("guilds.json") as f:
+#     guildsJson = json.load(f)
+
 
 token = "OTc0NjkwOTAwNjY2MTE4MjE1.GmogGT.hQCi7cfrP-FeWMxfTQcv2mqMHwS_dFIB2_FoBU"
 guild = discord.Object(974705743087431691)
@@ -13,24 +14,25 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix="unihub!", intents=intents, help_command=None, status=discord.Status.online, activity=discord.Game("UniHub"))
 
-with open("guilds.json", "w") as f:
-    guildsJson = json.load(f)
-    guildsJson.append({"id": f"{guild.id}"})
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+    guild_count = len(bot.guilds)
+    print(guild_count)
 
 @bot.event
 async def on_guild_join(guild=guild):
-    with open("guilds.json", "w") as f:
-        guildsJson = json.load(f)
-        guildsJson.append({""})
     print(guild.id)
 
 @bot.tree.command(guild=guild)
 async def create_event(interaction: discord.Interaction, category:Literal["Learning", "Gaming", "Dating"], name:str, max_user:int=None):
     await interaction.response.send_message(name)
+
+@bot.tree.command(guild=guild)
+async def help(interaction: discord.Interaction):
+    embed = discord.Embed(title = "Help page", description = "All accessible commannds", color = 0x1db546)
+    await interaction.response.send_message(embed=embed)
 
 @bot.command()
 async def sync(ctx):
