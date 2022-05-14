f4fd3b95c0c056635bab31baac4301d6992ff47e
monkaCode <f.h612@hotmail.com>
2022-05-14 03:17:45 +0200

    updated code

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from typing import Literal
 import discord
+from discord.ui import View, Button
 from discord.ext import commands
 import json
 
-# with open("guilds.json") as f:
-#     guildsJson = json.load(f)
+with open(r"guilds.json") as f:
+    guildsJson = json.load(f)
 
 
 # Counter that counts on how many servers the bot is connected
 guild_count = 0 
+class Color:
+    green = 0x1ac436
+    red = 0xc41a1a
+
 
 token = "OTc0NjkwOTAwNjY2MTE4MjE1.GmogGT.hQCi7cfrP-FeWMxfTQcv2mqMHwS_dFIB2_FoBU"
-guild = discord.Object(974705743087431691)
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -22,24 +26,56 @@ bot = commands.Bot(command_prefix="unihub!", intents=intents, help_command=None,
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     guild_count = len(bot.guilds)
-    print(guild_count)
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(f"connecting {guild_count} servers"))
 
 @bot.event
-async def on_guild_join(guild=guild):
-    print(guild.id)
+async def on_guild_join(guild):
+    x = len(guildsJson["servers"])
+    guildsJson["servers"][x] = {"guild": guild.id, "channel": 0}
+    with open("guilds.json", "w") as f:
+        json.dump(guildsJson, f)
 
-@bot.tree.command(guild=guild)
+@bot.tree.command()
 async def create_event(interaction: discord.Interaction, category:Literal["Learning", "Gaming", "Dating"], name:str, max_user:int=None):
-    await interaction.response.send_message(name)
+    guildMain = bot.get_guild(974705743087431691)
+    voiceChannel = await guildMain.create_voice_channel(name, category=discord.Object(974813975533482034))
+    inviteLink = await guildMain.get_channel(voiceChannel.id).create_invite(max_uses=max_user, unique=True)
+    button = Button(label="Event löschen", style=discord.ButtonStyle.red)
+    async def button_callback(interaction:discord.Interaction):
+        await guildMain.get_channel(voiceChannel.id).delete()
+        await interaction.response.send_message(embed=discord.Embed(title="Du hast diese Event gelöscht"), ephemeral=True)
 
-@bot.tree.command(guild=guild)
+    button.callback = button_callback
+    view = View()
+    view.add_item(button)
+    await interaction.response.send_message(embed=discord.Embed(title=":white_check_mark: Event wurde erstellt!", description=f"Name: **{name}**\nKategorie: **{category}**\nMaximale Nutzer: **{max_user}**\nHier beitreten: {inviteLink}", color=Color.green), view=view, ephemeral=True)
+    with open(r"guilds.json") as f:
+        guildsJson = json.load(f)
+    for x in range(len(guildsJson["servers"])):
+        await bot.get_guild(guildsJson["servers"][x]["guild"]).get_channel(guildsJson["servers"][x]["channel"]).send(embed=discord.Embed(title=name, description=f"Kategorie: {category}\nMaximale User: {max_user}\nBeitreten: {inviteLink}", color=Color.green))
+
+@bot.tree.command()
 async def help(interaction: discord.Interaction):
-    embed = discord.Embed(title = "Help page", description = "All accessible commannds", color = 0x1db546)
+    embed = discord.Embed(title = "Help page", description = "All accessible commannds", color=Color.green)
     await interaction.response.send_message(embed=embed)
+
+@bot.tree.command()
+async def set_info_channel(interaction:discord.Interaction, channelid:str):
+    channelid = int(channelid)
+    for x in range(len(guildsJson["servers"])):
+        if(guildsJson["servers"][x]["guild"] == interaction.guild_id):
+            guildsJson["servers"][x]["channel"] = channelid
+            break
+    with open("guilds.json", "w") as f:
+        try:
+            json.dump(guildsJson, f)
+            await interaction.response.send_message(embed=discord.Embed(title=":white_check_mark: UniHub-Info-Channel wurde gespeichert.", description=f"Der aktuelle UniHub-Info-Channel ist <#{channelid}>", color=Color.green))
+        except:
+            await interaction.response.send_message(embed=discord.Embed(title=":x: Etwas ist schiefgelaufen!", color=Color.red))
 
 @bot.command()
 async def sync(ctx):
-    print(await bot.tree.sync(guild=guild))
+    print(await bot.tree.sync())
     await ctx.send("Synced")
 
 bot.run(token)
